6fd60f33f9c7ce4a8e6bbd210eab0c1a9e80b4eb
patelvp <patel.visha@husky.neu.edu>
2018-04-22 13:43:39 -0400

    Added Excel files in evaluation.
    Evaluation results directory changed.

---
+++ Vishal Patel/Indexer/src/ToExcel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alienware
+ */
+public class ToExcel {
+    String ranknumber,docno,precision,recall;
+
+    public ToExcel(String ranknumber, String docno, String precision, String recall) {
+        this.ranknumber = ranknumber;
+        this.docno = docno;
+        this.precision = precision;
+        this.recall = recall;
+    }
+            
+}
